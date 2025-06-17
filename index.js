f725d40954e9d56d40e1f82050d3505fff2d8716
Ninah19h <jerolinaha@gmail.com>
2025-06-17 08:27:57 -0700

    Functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+// 1. Returns number of blocks from Scuber headquarters (42nd Street)
+function distanceFromHqInBlocks(someValue) {
+  return Math.abs(someValue - 42);
+}
+
+// 2. Returns number of feet from Scuber HQ (1 block = 264 feet)
+function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInBlocks(someValue) * 264;
+}
+
+// 3. Returns feet travelled between any two points
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
+}
+
+// 4. Returns the fare price based on distance travelled
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
